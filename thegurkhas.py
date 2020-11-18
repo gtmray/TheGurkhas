36e2a20fbbac3e0b63485dcee1072024a49765fd
Rewan Gautam <uni.rayone@gmail.com>
2020-11-18 12:13:55 +0545

    Added enemies creating and movement function

--- thegurkhas.py
+++ thegurkhas.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random
+from time import time, sleep
 
 pygame.init()
 scwidth = 500
@@ -13,9 +15,37 @@ upper_boundary=220
 width=40
 height=60
 velocity=10
-
 run = True
+
+enemy_count = 0
+enemies_list = []
+enemy_speed = 10
+e_width = 10
+e_height = 10
+
+time_current = time()
+
+def random_enemies_create(width, height):
+    global enemy_count
+    range_a, range_b = 220, 500
+    random_y = random.randint(range_a, range_b) # Randomize enemy position
+    x, y = 490, random_y
+
+    if enemy_count < 5:
+      enemies_list.append([x, y, width, height])
+      enemy_count += 1
+    print(enemies_list)
+    return enemies_list
+
+def enemies_movement(enemies_list, enemy_speed):
+  for items in enemies_list:
+    items[0] = items[0] - enemy_speed # Enemies movement to left
+    
+  for co_x, co_y, co_w, co_h in enemies_list:
+    pygame.draw.rect(win, (255, 255, 0), (co_x, co_y, co_w, co_h))
+
 while run:
+
   pygame.time.delay(100)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -37,6 +67,11 @@ while run:
       y += velocity
   
   win.fill((0, 0, 0))  
-    
+  
   pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+
+  if(round((time()-time_current))%5==0): # Creating enemies every 5 seconds
+    en_list = random_enemies_create(e_width, e_height)
+  
+  enemies_movement(en_list, enemy_speed)
   pygame.display.update()
